e5c708104090da467fbe449f72ee1a0ae1d75ebc
bluewind2017 <32268858+bluewind2017@users.noreply.github.com>
2020-09-19 22:07:12 +0900

    Update food.py

--- IBMWatsonVisualRecognitionAPI/food.py
+++ IBMWatsonVisualRecognitionAPI/food.py
@@ -2,13 +2,12 @@ import json
 from ibm_watson import VisualRecognitionV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator('0Q6XzIINBWyB4X-i6CViERgaew85Uxzio17bqbRym3f1')
+authenticator = IAMAuthenticator('Auto-generated service credentials_api_key'')
 visual_recognition = VisualRecognitionV3(
     version='2018-03-19',
     authenticator=authenticator
 )
-
-visual_recognition.set_service_url('https://api.kr-seo.visual-recognition.watson.cloud.ibm.com/instances/a5282502-54eb-4b8e-aadd-1c814ac28faa')
+visual_recognition.set_service_url('Auto-generated service credentials_URL')
 
 url = 'https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/fruitbowl.jpg'
 classifier_ids = ["food"]
