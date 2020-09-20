61b968970b92a87b54aa089c1b075f1a375719b4
bluewind2017 <32268858+bluewind2017@users.noreply.github.com>
2020-09-20 23:32:58 +0900

    Create CategorizeImage.py

---
+++ MicrosoftAzureCognitiveAPI/CategorizeImage.py
@@ -0,0 +1,44 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+#from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+import sys
+import time
+
+# Add your Computer Vision subscription key to your environment variables.
+if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
+    subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
+else:
+    print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+    sys.exit()
+# Add your Computer Vision endpoint to your environment variables.
+if 'COMPUTER_VISION_ENDPOINT' in os.environ:
+    endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
+else:
+    print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+    sys.exit()
+
+
+
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+remote_image_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"
+
+'''
+Describe an Image - remote
+This example describes the contents of an image with the confidence score.
+'''
+print("===== Describe an image - remote =====")
+# Call API
+description_results = computervision_client.describe_image(remote_image_url )
+
+# Get the captions (descriptions) from the response, with confidence level
+print("Description of remote image: ")
+if (len(description_results.captions) == 0):
+    print("No description detected.")
+else:
+    for caption in description_results.captions:
+        print("'{}' with confidence {:.2f}%".format(caption.text, caption.confidence * 100))
