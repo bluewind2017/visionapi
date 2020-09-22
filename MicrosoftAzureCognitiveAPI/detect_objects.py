184885b29d46adb96dddf7e8f956aadf6be9adb1
bluewind2017 <32268858+bluewind2017@users.noreply.github.com>
2020-09-23 08:27:52 +0900

    Add files via upload

---
+++ MicrosoftAzureCognitiveAPI/detect_objects.py
@@ -0,0 +1,40 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+#from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+import sys
+import time
+
+# Add your Computer Vision subscription key to your environment variables.
+if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
+    subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
+else:
+    print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+    sys.exit()
+# Add your Computer Vision endpoint to your environment variables.
+if 'COMPUTER_VISION_ENDPOINT' in os.environ:
+    endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
+else:
+    print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+    sys.exit()
+
+
+
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+# Get URL image with different objects
+remote_image_url_objects = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/objects.jpg"
+# Call API with URL
+detect_objects_results_remote = computervision_client.detect_objects(remote_image_url_objects)
+# Print detected objects results with bounding boxes
+print("Detecting objects in remote image:")
+if len(detect_objects_results_remote.objects) == 0:
+    print("No objects detected.")
+else:
+    for object in detect_objects_results_remote.objects:
+        print("object at location {}, {}, {}, {}".format( \
+        object.rectangle.x, object.rectangle.x + object.rectangle.w, \
+        object.rectangle.y, object.rectangle.y + object.rectangle.h))
