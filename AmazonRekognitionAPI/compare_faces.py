610cfcfe07919a05317e7b64b3b3e55931b49764
bluewind2017 <32268858+bluewind2017@users.noreply.github.com>
2020-09-22 14:43:42 +0900

    Create compare_faces.py

---
+++ AmazonRekognitionAPI/compare_faces.py
@@ -0,0 +1,25 @@
+import boto3
+def compare_faces(sourceFile, targetFile):
+    client=boto3.client('rekognition')
+    imageSource=open(sourceFile,'rb')
+    imageTarget=open(targetFile,'rb')
+    response=client.compare_faces(SimilarityThreshold=80,
+                                  SourceImage={'Bytes': imageSource.read()},
+                                  TargetImage={'Bytes': imageTarget.read()})
+    for faceMatch in response['FaceMatches']:
+        position = faceMatch['Face']['BoundingBox']
+        similarity = str(faceMatch['Similarity'])
+        print('The face at ' +
+               str(position['Left']) + ' ' +
+               str(position['Top']) +
+               ' matches with ' + similarity + '% confidence')
+    imageSource.close()
+    imageTarget.close()     
+    return len(response['FaceMatches'])          
+def main():
+    source_file='source'
+    target_file='target'
+    face_matches=compare_faces(source_file, target_file)
+    print("Face matches: " + str(face_matches))
+if __name__ == "__main__":
+    main()
