562e2d10d1dd15b7ce84f8da8fd63f4181e36ba5
bluewind2017 <32268858+bluewind2017@users.noreply.github.com>
2020-09-22 13:08:11 +0900

    Create face_pillow.py

---
+++ MicrosoftAzureCognitiveAPI/face_pillow.py
@@ -0,0 +1,56 @@
+""" Code source: https://docs.microsoft.com/en-au/azure/cognitive-services/face/quickstarts/python-sdk"""
+import asyncio
+import io
+import glob
+import os
+import sys
+import time
+import uuid
+import requests
+from urllib.parse import urlparse
+from io import BytesIO
+from PIL import Image, ImageDraw
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
+
+# Set the FACE_SUBSCRIPTION_KEY environment variable with your key as the value.
+# This key will serve all examples in this document.
+KEY = os.environ['FACE_SUBSCRIPTION_KEY']
+
+# Set the FACE_ENDPOINT environment variable with the endpoint from your Face service in Azure.
+# This endpoint will be used in all examples in this quickstart.
+ENDPOINT = os.environ['FACE_ENDPOINT']
+
+# Create an authenticated FaceClient.
+face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+# Detect a face in an image that contains a single face
+single_face_image_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+single_image_name = os.path.basename(single_face_image_url)
+detected_faces = face_client.face.detect_with_url(url=single_face_image_url)
+if not detected_faces:
+    raise Exception('No face detected from image {}'.format(single_image_name))
+
+# Convert width height to a point in a rectangle
+def getRectangle(faceDictionary):
+    rect = faceDictionary.face_rectangle
+    left = rect.left
+    top = rect.top
+    bottom = left + rect.height
+    right = top + rect.width
+    return ((left, top), (bottom, right))
+
+
+# Download the image from the url
+response = requests.get(single_face_image_url)
+img = Image.open(BytesIO(response.content))
+
+# For each face returned use the face rectangle and draw a red box.
+print('Drawing rectangle around face... see popup for results.')
+draw = ImageDraw.Draw(img)
+for face in detected_faces:
+    draw.rectangle(getRectangle(face), outline='red')
+
+# Display the image in the users default image browser.
+img.show()
