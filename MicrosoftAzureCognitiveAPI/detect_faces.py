f087cca1b60461ce177ae406429c349f0e22121d
bluewind2017 <32268858+bluewind2017@users.noreply.github.com>
2020-09-23 08:30:16 +0900

    Add files via upload

---
+++ MicrosoftAzureCognitiveAPI/detect_faces.py
@@ -0,0 +1,44 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+#from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+import sys
+import time
+
+# Add your Computer Vision subscription key to your environment variables.
+if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
+    subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
+else:
+    print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+    sys.exit()
+# Add your Computer Vision endpoint to your environment variables.
+if 'COMPUTER_VISION_ENDPOINT' in os.environ:
+    endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
+else:
+    print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+    sys.exit()
+
+
+
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+# Get an image with faces
+remote_image_url_faces = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/faces.jpg"
+# Select the visual feature(s) you want.
+
+remote_image_features = ["faces"]
+# Call the API with remote URL and features
+detect_faces_results_remote = computervision_client.analyze_image(remote_image_url_faces, remote_image_features)
+# Print the results with gender, age, and bounding box
+print("Faces in the remote image: ")
+if (len(detect_faces_results_remote.faces) == 0):
+    print("No faces detected.")
+else:
+    for face in detect_faces_results_remote.faces:
+        print("'{}' of age {} at location {}, {}, {}, {}".format(face.gender, face.age,face.face_rectangle.left, face.face_rectangle.top, \
+        face.face_rectangle.left + face.face_rectangle.width, \
+        face.face_rectangle.top + face.face_rectangle.height))
